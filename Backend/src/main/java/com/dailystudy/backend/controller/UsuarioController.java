6633ffcb7ec2d56aff6128784a41db60ba17ea3d
PE-zc <pauloernesto2004@gmail.com>
2026-05-04 15:29:48 -0300

    Alterando o controller

--- Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
+++ Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.dailystudy.backend.controller;
 import com.dailystudy.backend.dto.UsuarioRegistro;
 import com.dailystudy.backend.model.Usuario;
 import com.dailystudy.backend.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,17 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/usuario")
+@RequestMapping("/api/usuarios")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
 
     @PostMapping("/registro") // Mapeia a requisicao HTTP para criar um novo usuario no banco de dados
-    public ResponseEntity<Usuario> registro(@RequestBody UsuarioRegistro dto) {
-        Usuario newUsername = usuarioService.registroUsuario(dto);
+    public ResponseEntity<String> registrar(@Valid @RequestBody UsuarioRegistro dto) {
+        usuarioService.registroUsuario(dto);
 
-        return ResponseEntity.ok(newUsername);
-
+        return ResponseEntity.ok("Usuário registrado com sucesso!");
     }
 }
