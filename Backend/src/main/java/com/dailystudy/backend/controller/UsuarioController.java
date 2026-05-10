7dacaf56ada1ac5dbb0ce7e6e2aa4e032760acfa
PE-zc <pauloernesto2004@gmail.com>
2026-05-10 09:31:22 -0300

    Alterando o @Autowired por um construtor

--- Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
+++ Backend/src/main/java/com/dailystudy/backend/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import com.dailystudy.backend.dto.UsuarioRegistro;
 import com.dailystudy.backend.model.Usuario;
 import com.dailystudy.backend.service.UsuarioService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
 
-    @Autowired
-    private UsuarioService usuarioService;
+    private final UsuarioService usuarioService;
 
     @PostMapping("/registro") // Mapeia a requisicao HTTP para criar um novo usuario no banco de dados
     public ResponseEntity<String> registrar(@Valid @RequestBody UsuarioRegistro dto) {
